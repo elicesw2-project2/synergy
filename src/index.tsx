b589e69835f804b318c0e422746181cf4d055e39
ctd <qpflapffhs76@gmail.com>
2022-06-02 23:58:33 +0900

    Settings: Styled Reset 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
+import { Reset } from 'styled-reset';
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <BrowserRouter>
+    <Reset />
     <App />
   </BrowserRouter>
 );
