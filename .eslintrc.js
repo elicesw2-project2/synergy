08677a260ab5829d166c76f2d5728ab08ff55f64
siyah <sight41siyah@gmail.com>
2022-06-11 01:03:02 +0900

    Setting: elint. prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+};
--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
+  res.send('형욱 바보');
 });
 
 app.listen(port, () => {
