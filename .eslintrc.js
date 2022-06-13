55cd89234adfd50b1cdfd0c170ee5d9d67b035e5
Hye-Ji Na <nhj7911@gmail.com>
2022-06-13 23:38:40 +0900

    Setting : mysql db connection

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,14 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'import/extensions': 'off',
   },
   ignorePatterns: ['dist/', 'node_modules/'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
