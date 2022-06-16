3521342dedb5a7f998b3a18ba08148a0f7475b66
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-16 22:51:19 +0900

    로그인 회원가입 useForm

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import 'styles/Users/Login.scss';
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="login">
       <section className="login_container">
         <h1 className="login_title">SYNERGY</h1>
-        <form className="login_info">
+        <form
+          className="login_info"
+          onSubmit={handleSubmit(() => console.log('asdf'))}
+        >
           <hr />
-          <p>
-            <input type="text" placeholder="이메일" />
-          </p>
-          <p>
-            <input type="password" placeholder="비밀번호" />
-          </p>
+          <div>
+            <input
+              type="text"
+              {...register('id', { required: '아이디를 입력하세요.' })}
+              placeholder="이메일"
+            />
+            {errors.id && (
+              <div className="login_errors">{errors.id.message}</div>
+            )}
+          </div>
+          <div>
+            <input
+              type="password"
+              {...register('pw', { required: '비밀번호를 입력하세요.' })}
+              placeholder="비밀번호"
+            />
+            {errors.pw && (
+              <div className="login_errors">{errors.pw.message}</div>
+            )}
+          </div>
           <p>
             <button type="submit">로그인</button>
           </p>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import 'styles/Users/SignUp.scss';
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="signUp">
       <section className="signUp_container">
         <h1 className="signUp_title">SYNERGY</h1>
-        <form className="signUp_info">
+        <form
+          className="signUp_info"
+          onSubmit={handleSubmit(() => console.log('asdf'))}
+        >
           <hr />
           <p>
-            <input placeholder="이메일" />
+            <input
+              {...register('id', { required: '아이디를 입력하세요.' })}
+              placeholder="이메일"
+            />
+            {errors.id && (
+              <div className="login_errors">{errors.id.message}</div>
+            )}
           </p>
           <p>
-            <input placeholder="별명 (최대 8자 이하)" />
+            <input
+              {...register('nickname', { required: '필수정보 입니다.' })}
+              placeholder="별명 (최대 8자 이하)"
+            />
+            {errors.nickname && (
+              <div className="login_errors">{errors.nickname.message}</div>
+            )}
           </p>
           <p>
-            <input type="password" placeholder="비밀번호 (최소 5자 이상)" />
+            <input
+              {...register('pw', { required: '아이디를 입력하세요.' })}
+              type="password"
+              placeholder="비밀번호 (최소 5자 이상)"
+            />
+            {errors.pw && (
+              <div className="login_errors">{errors.pw.message}</div>
+            )}
           </p>
           <p>
-            <input type="password" placeholder="비밀번호 확인" />
+            <input
+              {...register('pwCheck', { required: '아이디를 입력하세요.' })}
+              type="password"
+              placeholder="비밀번호 확인"
+            />
+            {errors.pwCheck && (
+              <div className="login_errors">{errors.pwCheck.message}</div>
+            )}
           </p>
           <p>
             <button type="submit">회원가입</button>
