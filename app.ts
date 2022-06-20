3fc99e06b6388b2eecdd9d06dee823ad26711ffe
siyah <sight41siyah@gmail.com>
2022-06-21 00:02:46 +0900

    Setting: cors 수정, s3 config 수정

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -20,8 +20,6 @@ app.use('/image', imageRouter);
 app.get('/', (req: Request, res: Response) => {
   res.send('형욱 바보');
 });
-
-app.use(cors());
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
