791fd48f0d569abbdbb173c9abf3f93aa39780d9
user <yenje7@naver.com>
2022-06-21 00:49:39 +0900

    socketIO 수정

--- socket.ts
+++ socket.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/no-import-module-exports
 import { Server } from 'socket.io';
 import * as http from 'http';
+import path from 'path';
 
 const webSocket = (server: http.Server) => {
-  const io = new Server(server);
-
+  const io = new Server(server, { path: '/api' });
+  // io.path('/api');
   const users = [];
 
   function countRoom(roomIdx: string) {
@@ -13,12 +14,14 @@ const webSocket = (server: http.Server) => {
 
   // 소켓 연결
   io.on('connection', (socket: any) => {
+    socket['nickname'] = 'Anonymous';
     console.log('connected!');
 
-    socket.on('enter_room', (roomIdx: string) => {
+    socket.on('enter_room', (roomIdx: number, nickname: string) => {
       socket.join(roomIdx);
-      console.log(`${roomIdx} 입장`);
-      //socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 하나의 소켓에만 메시지 보냄
+      console.log(`${roomIdx}번방 입장`);
+      socket.to(roomIdx).emit('welcome', nickname);
+      console.log('???');
       // io.sockets.emit("room_change", publicRooms()); // 메시지를 모든 소켓에 보냄
     });
 
@@ -26,16 +29,12 @@ const webSocket = (server: http.Server) => {
     socket.emit('usercount', io.engine.clientsCount);
 
     // 메시지 수신
-    socket.on('message', function (message: any) {
-      console.log(message);
+    socket.on('message', function (message: string, nickname: string) {
+      socket['nickname'] = nickname;
+      console.log(`${socket.nickname}: ${message}`);
       // io.emit으로 연결된 모든 소켓들에 신호를 보냄
       io.emit('message', `${socket.nickname}: ${message}`);
     });
-
-    socket.on(
-      'nickname',
-      (nickname: string) => (socket['nickname'] = nickname)
-    );
 
     // 소켓 나가기
     socket.on('disconnecting', () => {
