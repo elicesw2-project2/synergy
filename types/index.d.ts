76dad90939cbb1ab67b46d33785183974295dc70
user <yenje7@naver.com>
2022-06-21 02:51:33 +0900

    Fix: index.d.ts 파일 수정

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,17 @@
-declare module 'engine.io-parser';
+declare module 'engine.io-parser' {
+  export type PacketType =
+    | 'open'
+    | 'close'
+    | 'ping'
+    | 'pong'
+    | 'message'
+    | 'upgrade'
+    | 'noop'
+    | 'error';
+  export type RawData = any;
+  export interface Packet {
+    type: PacketType;
+    options?: { compress: boolean };
+    data?: RawData;
+  }
+}
