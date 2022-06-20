b76d2ca166420cac718c14c262afedbe341de7e5
ctd <qpflapffhs76@gmail.com>
2022-06-20 20:03:13 +0900

    Setting: react-query 설치 및 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Reset } from 'styled-reset';
 
 import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Reset />
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Reset />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
