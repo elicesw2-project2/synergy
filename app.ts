4bafd0c93f61e402e42a0aa5d0f746794a70702c
siyah <sight41siyah@gmail.com>
2022-06-20 23:06:45 +0900

    Setting: cors 설정

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes';
+import cors from 'cors';
 
+import routes from './routes';
 import workspaceRouter from './routes/workspace.router';
 import imageRouter from './routes/image.routes';
 import { errorHandler } from './middlewares/errorHandler';
@@ -19,6 +20,8 @@ app.use('/image', imageRouter);
 app.get('/', (req: Request, res: Response) => {
   res.send('형욱 바보');
 });
+
+app.use(cors());
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
