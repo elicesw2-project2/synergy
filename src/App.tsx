6d6eefbe7ee00aa6224212327d13dc3289406dc2
ctd <qpflapffhs76@gmail.com>
2022-06-25 03:02:47 +0900

    Feat: 다중 라우팅 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-// Components
-import Bars from 'common/Bars';
-
-// Pages
-import Home from 'pages/Home';
-import CalenderBoard from 'pages/CalendarBoard';
-import Login from 'pages/Login';
-import SignUp from 'pages/SignUp';
 
 // recoil
 import { RecoilRoot } from 'recoil';
 
+// route
+import Router from 'Router';
+
 function App() {
   return (
     <RecoilRoot>
-      <Routes>
-        <Route element={<Bars />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/calenderBoard" element={<CalenderBoard />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} />
-      </Routes>
+      <Router />
     </RecoilRoot>
   );
 }
---
+++ src/components/WorkspaceHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'styles/Home.scss';
+
+function WorkspaceHome() {
+  return <div className="Home">워크스페이스에 오신 것을 환영합니다</div>;
+}
+
+export default WorkspaceHome;
