302b14032c346c63cf005708ded2a6a75f59a004
MinJuYoon <ymj3539@naver.com>
2022-06-27 22:41:54 +0900

    Feat: 로그인 확인 미들웨어 구현

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
+import { loginRequired } from '../middlewares/login.required';
 import * as userController from '../controllers/user.controller';
 
 const router = Router();
 
 // 유저 정보 조회
-router.get('/:id', userController.getUserById);
+router.get('/:id', loginRequired, userController.getUserById);
 
 // 유저 정보 수정
-router.patch('/:id', userController.setUser);
+router.patch('/:id', loginRequired, userController.setUser);
 
 // 유저 정보 삭제 (회원탈퇴)
-router.delete('/:id', userController.deleteUser);
+router.delete('/:id', loginRequired, userController.deleteUser);
 export default router;
