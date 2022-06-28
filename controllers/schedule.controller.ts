d741c74c2135f54818f1783abd7fabfb3f486744
siyah <sight41siyah@gmail.com>
2022-06-28 19:35:28 +0900

    Feature: scheduleCard API (미완)

--- controllers/schedule.controller.ts
+++ controllers/schedule.controller.ts
@@ -1,17 +1,113 @@
 import { Request, Response, NextFunction } from 'express';
-import findAllSchedules from '../services/schedule.service';
+import * as scheduleCardService from '../services/schedule.service';
 
-const getAllSchedules = async (res: Response, req: Request) => {
+export const getAllSchedules = async (
+  res: Response,
+  req: Request,
+  next: NextFunction
+) => {
   try {
-    const schedules = await findAllSchedules();
+    const schedules = await scheduleCardService.findAllSchedules();
     res.status(200).send({
       status: 200,
-      message: '채널 카테고리 목록 조회 성공',
+      message: '스케줄 카드 전체 목록 조회 성공',
       data: schedules,
     });
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 };
 
-export default getAllSchedules;
+export const getSchedulecardById = async (
+  res: Response,
+  req: Request,
+  next: NextFunction
+) => {
+  const scheduleId = Number(req.params.id);
+  try {
+    const scheduleCard = await scheduleCardService.findScheduleCardById(
+      scheduleId
+    );
+    res.status(200).send({
+      status: 200,
+      message: '스케줄 카드 조회 성공',
+      data: scheduleCard,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const setScheduleCard = async (
+  res: Response,
+  req: Request,
+  next: NextFunction
+) => {
+  try {
+    const schdeuldCardIdx = Number(req.params.id);
+    const scheduleCard = await scheduleCardService.setScheduleCard(
+      schdeuldCardIdx,
+      req.body
+    );
+    res.status(201).send({
+      status: 201,
+      message: '스케줄 카드 수정 성공',
+      data: scheduleCard,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createScheduleCard = async (
+  res: Response,
+  req: Request,
+  next: NextFunction
+) => {
+  try {
+    const schdeuldCardIdx = Number(req.params.id);
+    const scheduleCard = await scheduleCardService.setScheduleCard(
+      schdeuldCardIdx,
+      req.body
+    );
+    res.status(201).send({
+      status: 201,
+      message: '스케줄 카드 등록 성공',
+      data: scheduleCard,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteScheduleCard = async (
+  res: Response,
+  req: Request,
+  next: NextFunction
+) => {
+  try {
+    const scheduleCardIdx = Number(req.params.id);
+    await scheduleCardService.removeScheduleCard(scheduleCardIdx);
+    res.status(200).send({
+      status: 200,
+      message: '스케줄 카드 삭제 성공',
+      data: scheduleCardIdx,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+// async getAllDocument(req: Request, res: Response, next: NextFunction) {
+//   try {
+//     const documents = await documentService.findAllDocuments(
+//       Number(req.params.channel_idx)
+//     );
+//     res.status(200).send({
+//       status: 200,
+//       message: '채널별 문서 목록 조회 성공',
+//       data: documents,
+//     });
+//   } catch (err) {
+//     next(err);
+//   }
+// }
--- model/schedule.model.ts
+++ model/schedule.model.ts
@@ -1,6 +1,6 @@
 import db from './db';
 
-async function findAll() {
+export async function findAll() {
   return new Promise((resolve, reject) => {
     db.query('SELECT * FROM synergy.schedulecard;', (err, result) => {
       return err ? reject(err) : resolve(result);
@@ -8,4 +8,74 @@ async function findAll() {
   });
 }
 
-export default findAll;
+export async function findById(scheduleCardIdx: Number) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'SELECT * FROM synergy.schedulecard WHERE schedulecard_idx=?',
+      scheduleCardIdx,
+      (err, result) => {
+        return err ? reject(err) : resolve(result);
+      }
+    );
+  });
+}
+
+export async function create(
+  scheduleCardIdx: Number,
+  scheduleCardInfo: {
+    title: String;
+    category: String;
+    content: String;
+    duedate: Date;
+  }
+) {
+  return new Promise((resolve, reject) => {
+    const { title, category, content, duedate } = scheduleCardInfo;
+    db.query(
+      'INSERT INTO schedulecard SET title=?, category=?, content=?, duedate=?',
+      [title, category, content, duedate],
+      (err, result) => {
+        return err
+          ? reject(err)
+          : resolve({ scheduleCard_idx: scheduleCardIdx, ...scheduleCardInfo });
+      }
+    );
+  });
+}
+
+export async function update(
+  scheduleCardIdx: Number,
+  scheduleCardInfo: {
+    title: String;
+    category: String;
+    content: String;
+    duedate: Date;
+  }
+) {
+  return new Promise((resolve, reject) => {
+    const { title, category, content, duedate } = scheduleCardInfo;
+    db.query(
+      'UPDATE schedulecard SET title=?, category=?, content=?, duedate=?  WHERE schedulecard_idx=?',
+      [title, category, content, duedate, scheduleCardIdx],
+      (err, result) => {
+        return err
+          ? reject(err)
+          : resolve({ scheduleCard_idx: scheduleCardIdx, ...scheduleCardInfo });
+      }
+    );
+  });
+}
+
+export async function remove(scheduleCardIdx: Number) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'DELETE from schedulecard WHERE schedulecard_idx=?',
+      scheduleCardIdx,
+      (err, result) => {
+        return err
+          ? reject(err)
+          : resolve({ scheduleCard_idx: scheduleCardIdx });
+      }
+    );
+  });
+}
