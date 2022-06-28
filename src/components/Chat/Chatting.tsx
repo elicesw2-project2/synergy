db3aeb7a64f52669f06e71717b7d59902be6e3f2
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-29 01:25:43 +0900

    채팅기능
    방 하나로만 고정하고 채팅 가능한 상태

--- src/components/Chat/Chatting.tsx
+++ src/components/Chat/Chatting.tsx
@@ -1,55 +1,61 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import 'styles/Chat/Chatting.scss';
 
 const socket = io('/');
-console.log(socket);
-socket.emit('message', '내용', '닉네임', 1);
-socket.on('message', (message) => {
-  console.log(message);
-});
 
+// 채팅내용 컴포넌트
 function ChatContent(prop: any) {
   const { content } = prop;
-  console.log(content);
   return (
     <div className="chat_content">
       <ul>
-        <li>asdf</li>
-        {/* {content.map((el: any) => {
-          console.log(el);
-        })} */}
-        <li>{content[0]}</li>
+        {content.map((el: any) => {
+          return <li>{el}</li>;
+        })}
       </ul>
     </div>
   );
 }
 
+// 채팅 입력 컴포넌트
 function ChatInput() {
   const contentRef = useRef<HTMLTextAreaElement | null>(null);
   const [text, setText] = useState<string | undefined>('');
   const [content, setContent] = useState<any>([]);
 
+  // 채팅 전송 submit
   function chatSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    setContent([...content, contentRef?.current?.value]);
-    setText(contentRef?.current?.value);
+    const newChat = contentRef?.current?.value;
+    socket.emit('message', newChat, '닉네임', 1);
+    setText('');
   }
-
-  function selectOne() {
-    socket.emit('enter_room', 1, 'nickname');
-    console.log('asdf');
+  // 방 입장
+  function roomConnect() {
+    socket.emit('enter_room', 1, '닉네임');
+    console.log('방입장');
   }
+  // useEffect(() => {
+  socket.on('message', (message) => {
+    setContent([...content, message]);
+    console.log(message);
+  });
+  // }, []);
 
   return (
     <>
-      <ChatContent content={content} />
-      <button type="button" onClick={selectOne}>
+      <button type="button" onClick={roomConnect}>
         1번방
       </button>
+      <ChatContent content={content} />
       <div className="chat_inputBar">
         <form className="chat_info" onSubmit={chatSubmit}>
-          <textarea ref={contentRef} />
+          <textarea
+            ref={contentRef}
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
           <button type="submit">전송</button>
         </form>
       </div>
