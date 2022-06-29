3130010b7f3853fb2824e3cf2910db7474030c67
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-06-30 01:14:59 +0900

    Update Chat.tsx

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -14,41 +14,43 @@ function Chat() {
   const [chatView, setChatView] = useState<boolean>(false);
 
   return (
-    <div className="chat_box">
-      {chatView ? (
-        <div className="chat_unroll">
-          <div className="chat_bar">
-            <div className="chat_title">
-              <h2>땡땡의 채팅방</h2>
+    <>
+      <div className="chat_box">
+        {chatView ? (
+          <div className="chat_unroll">
+            <div className="chat_bar">
+              <div className="chat_title">
+                <h2>땡땡의 채팅방</h2>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="chat_close"
+                  onClick={() => {
+                    setChatView(!chatView);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              </div>
             </div>
-            <div>
-              <button
-                type="submit"
-                className="chat_close"
-                onClick={() => {
-                  setChatView(!chatView);
-                }}
-              >
-                <FontAwesomeIcon icon={faXmark} />
-              </button>
-            </div>
+            {/* <ChatContent /> */}
+            <ChatInput />
           </div>
-          {/* <ChatContent /> */}
-          <ChatInput />
-        </div>
-      ) : (
-        <button
-          type="submit"
-          className="chat_roll"
-          onClick={() => {
-            setChatView(!chatView);
-          }}
-        >
-          채팅창 작아진 상태
-        </button>
-      )}
+        ) : (
+          <button
+            type="submit"
+            className="chat_roll"
+            onClick={() => {
+              setChatView(!chatView);
+            }}
+          >
+            채팅창 작아진 상태
+          </button>
+        )}
+      </div>
       <Outlet />
-    </div>
+    </>
   );
 }
 
