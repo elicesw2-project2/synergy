ffd24b2328b0d6d73aef36d67f5a6e69589650be
ctd <qpflapffhs76@gmail.com>
2022-07-01 21:25:13 +0900

    Test: 테스트 커밋

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,6 +12,7 @@ export async function getWorkspaces() {
       Authorization: `Bearer ${localStorage.getItem('TOKEN')}` || 'not found',
     },
   });
+  console.log('test');
   return result.data.data;
 }
 
