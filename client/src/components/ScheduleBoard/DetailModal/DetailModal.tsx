40c6ea7d2dc0a4986e9a085f01f8c89bc9b8cf4f
ctd <qpflapffhs76@gmail.com>
2022-07-06 23:03:58 +0900

    [#5] 스케줄 카드 상세 페이지에서 제목 및 내용 수정 기능 구현

--- client/src/components/ScheduleBoard/DetailModal/DetailModal.tsx
+++ client/src/components/ScheduleBoard/DetailModal/DetailModal.tsx
@@ -1,7 +1,11 @@
 import { IScheduleCard } from 'pages/ScheduleBoard/ScheduleBoard';
-import React from 'react';
-import styles from './DetailModal.module.scss';
+import React, { useCallback, useState } from 'react';
 import 'styles/base/Modal.module.scss';
+import { useMutation, useQueryClient } from 'react-query';
+import { patchScheduleCard } from 'utils/api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
+import styles from './DetailModal.module.scss';
 
 interface IProps {
   onClickToggleModal: () => void;
@@ -9,11 +13,114 @@ interface IProps {
 }
 
 function DetailModal({ onClickToggleModal, card }: IProps) {
+  const [content, setContent] = useState(card.content);
+  const [isOpenContent, setIsOpenContent] = useState<boolean>(false);
+  const onClickToggleContent = useCallback(() => {
+    setIsOpenContent(!isOpenContent);
+  }, [isOpenContent]);
+
+  const handleChangeContent = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setContent(e.currentTarget.value);
+  };
+
+  const [title, setTitle] = useState<string>(card.title);
+  const [isOpenTitle, setIsOpenTitle] = useState<boolean>(false);
+  const onClickToggleTitle = useCallback(() => {
+    setIsOpenTitle(!isOpenTitle);
+  }, [isOpenTitle]);
+
+  const handleChangeTitle = (e: React.FormEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  const queryClient = useQueryClient();
+  const patchMutation = useMutation(patchScheduleCard, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('scheduleCards');
+    },
+  });
+
+  const handleUpdateContent = () => {
+    patchMutation.mutate({
+      schedulecard_idx: card.schedulecard_idx,
+      title,
+      content,
+    });
+    onClickToggleContent();
+  };
+
   return (
     <div className={styles.background}>
       <div className={styles.custom_container}>
         <form className={styles.form}>
           {/* 나가기 버튼 */}
+          <div className={styles.input_container}>
+            {isOpenTitle ? (
+              <>
+                <input
+                  type="text"
+                  className={styles.issue_title}
+                  value={title}
+                  onChange={handleChangeTitle}
+                />
+                <div>
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={styles.title_save_button}
+                    onClick={handleUpdateContent}
+                  />
+                  <FontAwesomeIcon
+                    icon={faX}
+                    className={styles.title_close_button}
+                    onClick={onClickToggleTitle}
+                  />
+                </div>
+              </>
+            ) : (
+              <button
+                type="button"
+                className={`${styles.issue_title} ${styles.issue_title_button}`}
+                onClick={onClickToggleTitle}
+              >
+                {card.title}
+              </button>
+            )}
+
+            <span className={styles.description}>설명</span>
+            {isOpenContent ? (
+              <div>
+                <textarea value={content} onChange={handleChangeContent} />
+                <button
+                  type="button"
+                  onClick={handleUpdateContent}
+                  className={styles.content_save_button}
+                >
+                  저장
+                </button>
+                <button
+                  type="button"
+                  onClick={onClickToggleContent}
+                  className={styles.content_close_button}
+                >
+                  닫기
+                </button>
+              </div>
+            ) : (
+              <button
+                type="button"
+                onClick={onClickToggleContent}
+                className={styles.add_button}
+              >
+                설명 추가하기...
+              </button>
+            )}
+          </div>
+          <div className={styles.detail_container}>
+            <div>상태: {card.category}</div>
+            <div>
+              <span>작성자: {card.nickname}</span>
+            </div>
+          </div>
           <button
             type="button"
             onClick={onClickToggleModal}
@@ -21,14 +128,6 @@ function DetailModal({ onClickToggleModal, card }: IProps) {
           >
             X
           </button>
-          <div className={styles.input_container}>
-            <span className={styles.issue_title}>{card.title}</span>
-            <span>{card.category}</span>
-            <span>{card.content}</span>
-            <textarea />
-          </div>
-          {/* 서버 추가 버튼 */}
-          <input type="submit" className={styles.submit_button} value="제출" />
         </form>
       </div>
     </div>
